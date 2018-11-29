d71a558487cbc2d99c26067dd715cf03da4f8aed
cornerskyless <573196853@qq.com>
2018-11-29 10:48:18 +0800

    修改打包问题

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,8 @@
 import Geelist from "./geelist.vue";
-import Vue from "vue";
-import ElementUI from "element-ui";
-import "element-ui/lib/theme-chalk/index.css";
 
-Vue.use(ElementUI);
 const GL = {
   install(Vue: any) {
-    Vue.component(Geelist.name, Geelist);
+    Vue.component('geelist', Geelist);
   }
 };
 export default GL;
